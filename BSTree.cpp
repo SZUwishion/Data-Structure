a726534ce750e9b28a9b6a3ecef3f1fb41931bee
Wu YuXuan <148472521+SZUwishion@users.noreply.github.com>
2023-12-05 13:54:05 +0800

    Update BSTree.cpp

--- BSTree.cpp
+++ BSTree.cpp
@@ -1,101 +1,112 @@
 #include <iostream>
 using namespace std;
 
-struct BSTree {
+struct BSTree
+{
   int data;
   BSTree *left;
   BSTree *right;
 };
 
-bool Search(BSTree *T, int key, BSTree *pre, BSTree **p) {
-  if (!T) {
-    *p = pre;
+bool Search(BSTree *Node, int key, BSTree *pre, BSTree **temp)
+{
+  if (!Node) {
+    *temp = pre;
     return false;
-  } else if (key == T->data) {
-    *p = T;
+  } else if (key == Node->data) {
+    *temp = Node;
     return true;
-  } else if (key < T->data)
-    return Search(T->left, key, T, p);
-  else
-    return Search(T->right, key, T, p);
+  } else if (key < Node->data){
+    return Search(Node->left, key, Node, temp);
+  } else {
+    return Search(Node->right, key, Node, temp);
+  }
 }
 
-bool Insert(BSTree **T, int e) {
-  BSTree *p = new BSTree();
-  if (!Search(*T, e, NULL, &p)) {
-    BSTree *s = new BSTree();
-    s->data = e;
-    s->left = s->right = NULL;
-    if (!p)
-      *T = s;
-    else if (e < p->data)
-      p->left = s;
-    else
-      p->right = s;
+bool Insert(BSTree **Node, int element)
+{
+  BSTree *temp = new BSTree();
+  if (!Search(*Node, element, NULL, &temp)) {
+    BSTree *newtemp = new BSTree();
+    newtemp->data = element;
+    newtemp->left = newtemp->right = NULL;
+    if (!temp) {
+      *Node = newtemp;
+    } else if (element < temp->data) {
+      temp->left = newtemp;
+    } else {
+      temp->right = newtemp;
+    }
     return true;
   }
   return false;
 }
 
-bool Delete(BSTree **T) {
-  BSTree *p, *s;
-  if (!(*T)->left && !(*T)->right)
-    *T = NULL;
-  else if (!(*T)->left) {
-    p = *T;
-    *T = (*T)->right;
-    delete (p);
-  } else if (!(*T)->right) {
-    p = *T;
-    *T = (*T)->left;
-    delete (p);
+bool Delete(BSTree **Node)
+{
+  BSTree *temp1, *temp2;
+  if (!(*Node)->left && !(*Node)->right){
+    *Node = NULL;
+  } else if (!(*Node)->left) {
+    temp1 = *Node;
+    *Node = (*Node)->right;
+    delete (temp1);
+  } else if (!(*Node)->right) {
+    temp1 = *Node;
+    *Node = (*Node)->left;
+    delete (temp1);
   } else {
-    p = *T;
-    s = p->left;
-    while (s->right) {
-      p = s;
-      s = s->right;
+    temp1 = *Node;
+    temp2 = temp1->left;
+    while (temp2->right) {
+      temp1 = temp2;
+      temp2 = temp2->right;
     }
-    (*T)->data = s->data;
-    if (p != *T)
-      p->right = s->left;
-    else
-      p->left = s->left;
-    delete (s);
+    (*Node)->data = temp2->data;
+    if (temp1 != *Node) {
+      temp1->right = temp2->left;
+    } else {
+      temp1->left = temp2->left;
+    }
+    delete (temp2);
   }
   return true;
 }
 
-bool DeleteBST(BSTree **T, int key) {
-  if (!T)
+bool DeleteBST(BSTree **Node, int key)
+{
+  if (!Node) {
     return true;
-  else {
-    if (key == (*T)->data) {
-      Delete(&*T);
+  } else {
+    if (key == (*Node)->data) {
+      Delete(&*Node);
       return false;
-    } else if (key < (*T)->data)
-      return DeleteBST(&(*T)->left, key);
-    else
-      return DeleteBST(&(*T)->right, key);
+    } else if (key < (*Node)->data) {
+      return DeleteBST(&(*Node)->left, key);
+    } else {
+      return DeleteBST(&(*Node)->right, key);
+    }
   }
 }
 
-void MidOrder(BSTree *T) {
-  if (T == NULL)
+void MidOrder(BSTree *Node) {
+  if (Node == NULL) {
     return;
-  MidOrder(T->left);
-  cout << T->data << " ";
-  MidOrder(T->right);
+  }
+  MidOrder(Node->left);
+  cout << Node->data << " ";
+  MidOrder(Node->right);
 }
 
 int main() {
   int a[] = {21, 43, 11, 54, 30, 49, 8}; // test data
-  BSTree *T = new BSTree();
-  T = NULL;
-  for (int i = 0; i < 7; i++)
-    Insert(&T, a[i]);
-  MidOrder(T);
+  BSTree *Tree = new BSTree();
+  Tree = NULL;
+  for (int i = 0; i < 7; i++) {
+    Insert(&Tree, a[i]);
+  }
+  MidOrder(Tree);
   cout << endl;
-  DeleteBST(&T, 54);
-  MidOrder(T);
+  DeleteBST(&Tree, 54);
+  MidOrder(Tree);
 }
